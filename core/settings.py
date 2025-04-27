cf87777fb58e60014e023b50714c3f6efae4069f
Nivea <nc.chagasribeiro@gmail.com>
2025-04-27 11:50:30 -0300

    Alterações nas variáveis de ambiente

--- core/settings.py
+++ core/settings.py
@@ -13,11 +13,16 @@ from typing import Literal, Optional
 
 from pydantic import Field, validator
 from pydantic_settings import BaseSettings
-
+import os
 
 class Settings(BaseSettings):
     # ───────────── Segurança • JWT ─────────────
-    secret_key: str = Field(..., alias="ONEAPIIRS_SECRET_KEY")
+    SECRET_KEY = os.getenv('SECRET_KEY', 'fallback_secret')
+    DB_PASSWORD = os.getenv('DB_PASSWORD', 'fallback_password')
+    DB_USERNAME = os.getenv('DB_USERNAME', 'fallback_user')
+    DB_HOST = os.getenv('DB_HOST', 'fallback_host')
+    DB_PORT = os.getenv('DB_PORT', 'fallback_port')
+    DB_NAME = os.getenv('DB_NAME', 'fallback_db')
     public_key: Optional[str] = Field(None, alias="ONEAPIIRS_PUBLIC_KEY")
     algorithm: str = Field("HS256", alias="ONEAPIIRS_ALGORITHM")
     access_token_expire_minutes: int = Field(30, alias="ONEAPIIRS_TOKEN_EXPIRE_MIN")
